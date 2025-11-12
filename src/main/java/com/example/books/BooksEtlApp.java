984afa37da8bd6d7462bdb954b7210583d095fb8
albanomataj <albano.mataj@gmail.com>
2025-11-11 20:40:29 -0500

    integrate Tesseract

--- src/main/java/com/example/books/BooksEtlApp.java
+++ src/main/java/com/example/books/BooksEtlApp.java
@@ -9,11 +9,11 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
-import org.apache.camel.spring.boot.CamelSpringBootApplication;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
@@ -21,7 +21,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import tech.jhipster.config.DefaultProfileUtil;
 import tech.jhipster.config.JHipsterConstants;
 
-@CamelSpringBootApplication
+@SpringBootApplication
 @EnableKafka
 @EnableConfigurationProperties({ LiquibaseProperties.class, ApplicationProperties.class, KafkaTopicProperties.class })
 public class BooksEtlApp {
